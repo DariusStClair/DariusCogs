3748e83602688692e80dce5dfa1bb40ca58b5ff8
DariusStClair <>
2019-07-23 11:22:25 -0500

    Update cass.py

--- leaguehell/cass.py
+++ leaguehell/cass.py
@@ -4,7 +4,6 @@ from cassiopeia import Summoner
 cassiopeia.set_default_region("EUNE")
 
 class Cass:
-    def casskey(self, ctx, key: key):
+    def casskey(self):
         cassiopeia.set_riot_api_key(key)
         print(f"Key set to {key}")
-        
