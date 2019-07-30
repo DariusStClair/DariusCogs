87b3c5af6dbcd12babea04ac8d843d1c123f47e6
DariusStClair <>
2019-07-30 09:32:20 -0500

    HVH Stuff

---
+++ hvhtools/__init__.py
@@ -0,0 +1,4 @@
+from .hvhtools import Hvhtools
+
+def setup(bot):
+    bot.add_cog(Hvhtools(bot))
---
+++ hvhtools/hvhtools.py
@@ -0,0 +1,36 @@
+# Discord
+import discord
+# Red
+from redbot.core import checks, Config, commands
+from redbot.core.utils.chat_formatting import bold, box, inline
+# Libs
+
+class Hvhtools(commands.Cog):
+    """Tools for the HVH community"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @checks.guildowner()
+    @commands.group(autohelp=True)
+    async def hvh(self, ctx):
+        """Various tools for the HVH community"""
+        pass
+
+    @hvh.command()
+    async def rulesen(self, ctx):
+        """Embeds the rules in:
+        English"""
+        await ctx.send('Test en')
+
+    @hvh.command()
+    async def rulesru(self, ctx):
+        """Embeds the rules in:
+        Russian"""
+        await ctx.send('Test ru')
+
+    @hvh.command()
+    async def rulesbg(self, ctx):
+        """Embeds the rules in:
+        Bulgarian"""
+        await ctx.send('Test bg')
