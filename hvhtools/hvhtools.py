daddd439403d9dcaa2102b14b334c1387721374f
DariusStClair <>
2019-07-30 09:39:41 -0500

    Update hvhtools.py

--- hvhtools/hvhtools.py
+++ hvhtools/hvhtools.py
@@ -5,6 +5,8 @@ from redbot.core import checks, Config, commands
 from redbot.core.utils.chat_formatting import bold, box, inline
 # Libs
 
+footer = "Powered by HELL"
+
 class Hvhtools(commands.Cog):
     """Tools for the HVH community"""
 
@@ -20,7 +22,10 @@ class Hvhtools(commands.Cog):
     @hvh.command()
     async def rulesen(self, ctx):
         """Embeds the rules in: **English**"""
-        await ctx.send('Test en')
+        emen = discord.Embed(colour=15158332, description=":flag_us:")
+        emen.add_field(name="1. Be respectful and act civil.", value=u'\u200b', inline=False)
+        emen.set_footer(text=footer)
+        await ctx.send(emen)
 
     @hvh.command()
     async def rulesru(self, ctx):
