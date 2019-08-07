9913393fe7aecfb56683c3ab944b02b4f3c63517
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-08-07 10:35:15 +0300

    Update leaguehell.py

--- leaguehell/leaguehell.py
+++ leaguehell/leaguehell.py
@@ -220,9 +220,8 @@ class Leaguehell(commands.Cog):
                 break
             em = discord.Embed(colour=15158332)
             chname = await self.lib.get_champ_name(str(i["championId"]))
-            getchico = await self.lib.ddragon_champico(str(i["championId"]))
-            urlll ="url="
-            chico = urlll.join(getchico)
+            getchico = str(await self.lib.ddragon_champico(str(i["championId"])))
+            chico = getchico.replace("'", "")
             clvl = i["championLevel"]
             cpoints = i["championPoints"]
             cchest = i["chestGranted"]
