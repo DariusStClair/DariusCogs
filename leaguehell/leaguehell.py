05ce99776dfb341ad820ab9d995107a6e1504255
DariusStClair <>
2019-08-12 13:37:47 -0500

    Update leaguehell.py

--- leaguehell/leaguehell.py
+++ leaguehell/leaguehell.py
@@ -397,7 +397,7 @@ class Leaguehell(commands.Cog):
         #    em.description = emdesc
         #    em.set_footer(text=f"Powered by HELL | Requested by {author} | {vversion}")
         #await ctx.send(embed=em)
-        await ctx.send(data[0])
+        await ctx.send(data[0]["id"])
 
     @checks.is_owner()
     @commands.command(name="leaguepatch")
