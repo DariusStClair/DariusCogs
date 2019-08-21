8bce4eaeafd4c0009a88f133abef8be655cd275e
Darius0069 <39631501+Darius0069@users.noreply.github.com>
2019-08-21 04:09:29 +0300

    Update leaguehell.py

--- leaguehell/leaguehell.py
+++ leaguehell/leaguehell.py
@@ -231,7 +231,8 @@ class Leaguehell(commands.Cog):
         em = discord.Embed(colour=15158332)
         icostr = str(await self.lib.summ_icon(name, xreg))
         total = await self.lib.get_mastery(name, xreg)
-        emdesc = (f"{sumname} / {xreg}\nTotal mastery: {total}\nTop 3 champions by mastery in {xreg}:")
+        reg = xreg.upper
+        emdesc = (f"**{sumname}**\nTotal mastery: **{total}**\nTop 3 champions by mastery in **{reg}**:")
         em.description = emdesc
         em.url = icostr
         em.set_footer(text=(f"Powered by HELL | Requested by {author} | {vversion}"), icon_url=icostr)
