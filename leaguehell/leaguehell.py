95412021f0f939d2e2ae7890502b8832b4d13897
Darius St. Clair <52074319+DariusStClair@users.noreply.github.com>
2019-09-21 19:28:24 +0300

    Update leaguehell.py

--- leaguehell/leaguehell.py
+++ leaguehell/leaguehell.py
@@ -146,18 +146,18 @@ class Leaguehell(commands.Cog):
         message = await ctx.send(embed=em)
         aname = await self.config.member(user).Name()
         areg = await self.config.member(user).Region()
-        if aname != "None":
+        if aname == "None":
+            veigarq = "https://66.media.tumblr.com/098fef7a648679a31f25e33362b2602c/tumblr_on1g2lljht1tnb6cko3_250.gif"
+            em.set_thumbnail(url=veigarq)
+            emdesc = (f"Welp, that user doesn't have an account set")
+            em.description = emdesc
+        else:
             propername = await self.lib.get_prname(aname, areg)
             icostr = str(await self.lib.summ_icon(aname, areg))
             em.set_thumbnail(url=icostr)
             emdesc = (f"**{user}**'s summoner name(s):")
             em.description = emdesc
             em.add_field(name=f'**{propername}** ({areg})', value=u'\u200b')
-        else:
-            veigarq = "https://66.media.tumblr.com/098fef7a648679a31f25e33362b2602c/tumblr_on1g2lljht1tnb6cko3_250.gif"
-            em.set_thumbnail(url=veigarq)
-            emdesc = (f"Welp, that user doesn't have an account set")
-            em.description = emdesc
         await message.edit(embed=em)
 
     @league.command(pass_context=True, no_pm=True)
