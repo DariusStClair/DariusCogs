701e28fba1744751d5ae0291520b088f993ea6ee
DariusStClair <>
2019-10-03 08:27:35 -0500

    ZZZZZZZZZZZZZZg

--- dariustoolkit/dariustoolkit.py
+++ dariustoolkit/dariustoolkit.py
@@ -9,7 +9,7 @@ from redbot.core.utils.chat_formatting import bold, box, inline
 footer = "Powered by HELL"
 
 class Hvhtools(commands.Cog):
-    """Tools for the HVH community"""
+    """Tools for a few communities"""
 
     def __init__(self, bot):
         self.bot = bot
@@ -17,7 +17,7 @@ class Hvhtools(commands.Cog):
     @checks.guildowner()
     @commands.group(autohelp=True)
     async def hvh(self, ctx):
-        """Various tools for the HVH community"""
+        """Various tools for HVH"""
         pass
 
     @hvh.command()
@@ -38,7 +38,7 @@ class Hvhtools(commands.Cog):
 
     @hvh.command()
     async def rulesru(self, ctx):
-        """Embeds the rules in: **Russian**"""
+        """HVH: Embeds the rules in: **Russian**"""
         emen = discord.Embed(colour=15158332, description=":flag_ru:")
         emen.add_field(name="1. Будьте уважительны и действуйте вежливо.", value=u'\u200b', inline=False)
         emen.add_field(name="2. Используйте правильную грамматику и орфографию и не спамуйте.", value=u'\u200b', inline=False)
@@ -54,7 +54,7 @@ class Hvhtools(commands.Cog):
 
     @hvh.command()
     async def rulesbg(self, ctx):
-        """Embeds the rules in: **Bulgarian**"""
+        """HVH: Embeds the rules in: **Bulgarian**"""
         emen = discord.Embed(colour=15158332, description=":flag_bg:")
         emen.add_field(name="1. Дръжте се уважително и цивилизовано.", value=u'\u200b', inline=False)
         emen.add_field(name="2. Използвайте правилна граматика и правопис и не спамете.", value=u'\u200b', inline=False)
@@ -87,3 +87,37 @@ class Hvhtools(commands.Cog):
         embed.add_field(name=":flag_bg:", value=embg)
         embed.set_footer(text=footer)
         await ctx.send (embed=embed)
+
+    @checks.is_admin_or_superior()
+    @commands.group(autohelp=True)
+    async def vg(self, ctx):
+        """Various tools for VG"""
+        pass
+
+    @checks.is_admin_or_superior()
+    @hvh.command()
+    async def assignroles(self, ctx):
+        """Embeds the the assign roles list"""
+        emoji_eune = "<:vg_eune:452893495921737728>"
+        emoji_euw = "<:vg_euw:452893505778483201> "
+        emoji_top = "<:vg_top:452893972688404491> "
+        emoji_jungle = "<:vg_jungle:452893985107869737> "
+        emoji_mid = "<:vg_mid:452893993219391509> "
+        emoji_adc = "<:vg_adc:452894007509385216> "
+        emoji_supp = "<:vg_supp:452895441961615383> "
+        emoji_fill = "<:vg_fill:452893957215485953> "
+        emoji_csgo = "<:CSGOlogo:534489391557509141> "
+        emoji_fortnite = "<:si_fortnite:550324854532866088> "
+        emoji_mc = "<:minecraft:513431092834074625> "
+        emoji_pubg = "<:si_pubg:550324853836611585> "
+        emoji_wc3 = "<:wc3:629302335880822814> "
+        emoji_wow = "<:WoW:534489368228921354>"
+        emoji_tft = "<:tft_icon:594189213222699009>"
+        emoji_blank = "<:blank:438114864003809290>"
+        emen = discord.Embed(colour=15158332, description="Реактнете със съответното емоджи за да получите/премахнете съответната роля.")
+        emen.add_field(name="Изберете регион (или два)", value=f"EUNE - {emoji_eune}{emoji_blank}EUW - {emoji_euw}{emoji_blank}", inline=true)
+        emen.add_field(name="Изберете предпочитаните ви роли", value=f"Top - {emoji_top}{emoji_blank}Jungle - {emoji_jungle}{emoji_blank}Mid - {emoji_mid}{emoji_blank}\nADC - {emoji_adc}{emoji_blank}Support - {emoji_supp}{emoji_blank}Fill - {emoji_fill}{emoji_blank}", inline=true)
+        emen.add_field(name="Изберете игрите, които играете", value=f"CS:GO - {emoji_csgo}{emoji_blank}Fortnite - {emoji_fortnite}{emoji_blank}Minecraft - {emoji_mc}{emoji_blank}", inline=False)
+        emen.add_field(name=u'\u200b', value=f"PUBG - {emoji_pubg}{emoji_blank}WarCraft III - {emoji_wc3}{emoji_blank}WoW - {emoji_wow}{emoji_blank}Teamfight Tactics - {emoji_tft}{emoji_blank}", inline=False)
+        emen.set_footer(text=footer)
+        await ctx.send(embed=emen)
