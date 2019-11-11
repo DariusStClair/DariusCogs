5dfb9d019579b61c43aeb8eff97323400e99e59a
DariusStClair <>
2019-11-11 07:51:21 -0600

    Update infohell.py
    Facepalm

--- infohell/infohell.py
+++ infohell/infohell.py
@@ -15,7 +15,7 @@ class Infohell(commands.Cog):
     """The info Cog for Hell"""
 
     def __init__(self, bot):
-        bot.remove_command(avatar)
+        bot.remove_command("avatar")
         self.bot = bot
         self.dfooter = "Powered by Entropy"
         default_member = {
