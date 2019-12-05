be16eb7e16b9b117125ec92ae22ef4ff4be08426
DariusStClair <>
2019-12-05 12:18:23 -0600

    Create Hearthpebble and then be incredibly sad that I don't know what the fuck am I doing with my life and also I haven't touched python in way too long and sigh

---
+++ hearthpebble/hearthpebble.py
@@ -0,0 +1,62 @@
+import discord
+from discord.utils import get
+# Red stuffs
+from redbot.core import checks, Config, bank, commands
+from redbot.core.utils import mod
+from redbot.core.utils.menus import menu, start_adding_reactions, DEFAULT_CONTROLS
+from redbot.core.utils.chat_formatting import bold, box, inline
+# Libs
+import aiohttp
+import asyncio
+import datetime
+import random
+import re
+from typing import Union
+# HS stuffs
+from .hslib import Hslib
+
+vversion = "version: 0.00"
+deffooter = f"Powered by Entropy | {vversion}"
+
+def apikeycheck():
+    async def predicate(ctx):
+        key = await ctx.bot.db.api_tokens.get_raw("hearthpebble", default=None)
+        try:
+            result = True if key["hsapikey"] else False
+        except:
+            result = False
+        if not result and ctx.invoked_with in dir(ctx.bot.get_cog("Hearthpebble")):
+            await ctx.send("Yo, api key gotta be set first with the 'hsapikey <key>' command")
+        if ctx.channel.permissions_for(discord.utils.get(ctx.guild.members, id=ctx.bot.user.id)).add_reactions:
+            return result
+        else:
+            raise commands.ReactionsCheckFailure(message="I got no permissions to add reactions")
+    return commands.check(predicate)
+
+class Leaguehell(commands.Cog):
+    """Yet another shitty Hearthstone Cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.lib = Handler(bot)
+        default_global = {"hsapikey": None}
+        self.config = Config.get_conf(self, identifier=698731666, force_registration=True)
+        self.config.register_global(**default_global)
+
+    @checks.is_owner()
+    @commands.command(name="hsapikey")
+    async def leagueapi(self, ctx, *, key):
+        """Set a key to use the Hearthpebble commands"""
+        await self.bot.db.api_tokens.set_raw("hsapikey", value={'hsapikey': key})
+        await ctx.send(">>> Easy.")
+
+    @commands.group(name="hs", no_pm=True)
+    async def hs(self, ctx):
+        """Various Hearthstone commands"""
+        pass
+
+    @hs.command(pass_context=True, no_pm=True)
+    async def card(self, ctx, *, name):
+        """Search for a card"""
+        res = await self.lib._getcard(name)
+        await ctx.send(res)
---
+++ hearthpebble/hslib.py
@@ -0,0 +1,57 @@
+import aiohttp
+import asyncio
+from math import floor, ceil
+import datetime
+import discord
+from discord import utils
+from redbot.core import checks, Config, bank, commands
+
+class Hslib:
+    def __init__(self, bot):
+        self.bot = bot
+        # Rapidapi
+        self.rurl = "https://omgvamp-hearthstone-v1.p.rapidapi.com"
+        self.rapi = None
+        self.rkey = None
+        self.searchcardname = "/cards/search/{}"
+        self.card = "/cards/{}"
+        self.hsinfo = "/info"
+        self.headers = {
+            "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+            "X-RapidAPI-Key": None
+        }
+        self._session = aiohttp.ClientSession()
+        # cdn?
+
+    def cog_unload(self):
+        self.__unload()
+
+    async def __unload(self):
+        asyncio.get_event_loop().create_task(self._session.close())
+
+    async def _getapikey(self):
+        if not self.rkey:
+            db = await self.bot.db.api_tokens.get_raw("hsapikey", default=None)
+            self.rkey = db["hsapikey"]
+            return self.rkey
+        else:
+            return self.rkey
+
+    async def _updheaders(self):
+        if not self.headers["X-RapidAPI-Key"]:
+            self.headers["X-RapidAPI-Key"] = await self._getapikey()
+            return self.headers
+        else:
+            return self.headers
+    
+    async def _rq(self, rq):
+        headers = await self._updheaders
+        async with self._session.get(rq, headers=headers) as r:
+            return await r.json()
+
+    async def _getcard(self, name):
+        rq = self.rurl + self.card.format(name)
+        rj = await self._rq(rq)
+        return rj
+
+            
